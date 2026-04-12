3f4a84199c1418a3925e5cbd986e7eff0db174da
Doberto mr lit <jeandoberto55@gmail.com>
2026-04-12 12:00:17 -0700

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -10,7 +10,7 @@ async function githubCommand(sock, chatId, message) {
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
-    let txt = `*☆  QUEEN VALENTINA-MD  ☆*\n\n`;
+    let txt = `*☆  DOBERTO XD  ☆*\n\n`;
     txt += `✩  *Name* : ${json.name}\n`;
     txt += `✩  *Watchers* : ${json.watchers_count}\n`;
     txt += `✩  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
