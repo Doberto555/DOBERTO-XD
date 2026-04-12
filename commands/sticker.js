9f409fbf825856d0d9dae917c2a1ead73e3f9d49
Doberto mr lit <jeandoberto55@gmail.com>
2026-04-12 11:52:24 -0700

    Update sticker.js

--- commands/sticker.js
+++ commands/sticker.js
@@ -37,7 +37,7 @@ async function stickerCommand(sock, chatId, message) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363407561123100@newsletter',
-                    newsletterName: 'LASER-MD',
+                    newsletterName: 'DOBERTO XD',
                     serverMessageId: -1
                 }
             }
@@ -59,7 +59,7 @@ async function stickerCommand(sock, chatId, message) {
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363407561123100@newsletter',
-                        newsletterName: 'LASER-MD',
+                        newsletterName: 'DOBERTO XD',
                         serverMessageId: -1
                     }
                 }
@@ -217,7 +217,7 @@ async function stickerCommand(sock, chatId, message) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363407561123100@newsletter',
-                    newsletterName: 'LASER-MD',
+                    newsletterName: 'DOBERTO XD',
                     serverMessageId: -1
                 }
             }
